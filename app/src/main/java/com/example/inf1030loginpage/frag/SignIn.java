0ca98e28962db3150ab89409580ac82b8ed617e7
fashola <nurudeen.fashola@uqtr.ca>
2026-02-03 00:27:50 -0500

    ajout de la classe SingIn()

--- app/src/main/java/com/example/inf1030loginpage/frag/SignIn.java
+++ app/src/main/java/com/example/inf1030loginpage/frag/SignIn.java
@@ -1,11 +1,23 @@
 package com.example.inf1030loginpage.frag;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+
+import com.example.inf1030loginpage.R;
 
 public class SignIn extends Fragment {
 
     public SignIn (){
 
+    }
+    public View onCreateView (LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle saveInstanceState){
+        return inflater.inflate(R.layout.sign_in_fragment, container, false );
     }
 
 }
