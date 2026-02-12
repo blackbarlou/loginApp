bd69536202feba58dc6d43c9bcc8054d65bb7cac
fashola <nurudeen.fashola@uqtr.ca>
2026-02-11 23:07:53 -0500

    ajout de binding pour lier favoris_frag.xml a la classe FavFragment

--- app/src/main/java/com/example/inf1030loginpage/menuFrag/FavFragment.java
+++ app/src/main/java/com/example/inf1030loginpage/menuFrag/FavFragment.java
@@ -5,19 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.inf1030loginpage.MyAdaptater;
 import com.example.inf1030loginpage.R;
+import com.example.inf1030loginpage.databinding.FavorisFragBinding;
+
 public class FavFragment extends Fragment {
+
+    FavorisFragBinding binder;
+    MyAdaptater adaptater;
     public FavFragment(){
 
     }
 
-    public View onCreateView ( LayoutInflater inflater,
-                               ViewGroup container,
-                               Bundle saveInstanceState ){
-        return inflater.inflate(R.layout.favoris_frag,
+    public View onCreateView (@NonNull LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle saveInstanceState ){
+        binder = FavorisFragBinding.inflate( inflater,
                 container,
-                false);
+                false );
+        return binder.getRoot();
     }
 }
